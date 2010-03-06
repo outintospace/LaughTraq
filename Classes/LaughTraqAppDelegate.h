60b012c6642dedf934e7db054bfee5bfdb7a4a51
Rob Standefer <rob@standefer.com>
2010-03-06 10:42:34 -0600

    changed copyright message

--- Classes/LaughTraqAppDelegate.h
+++ Classes/LaughTraqAppDelegate.h
@@ -3,7 +3,7 @@
 //  LaughTraq
 //
 //  Created by Robert Standefer on 3/6/10.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//  Copyright Robert Standefer 2010. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
